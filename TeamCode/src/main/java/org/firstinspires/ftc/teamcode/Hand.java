0a16dd2275203ec8496bb35d8fe1e982b40e77b6
James Burnett <james.c.burnett@icloud.com>
2023-11-06 20:31:10 -0500

    fixed package

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hand.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.robot;
+package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
