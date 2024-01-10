ef4676b9899f25b41b6c281e2aea399c65a974f3
James Burnett <james.c.burnett@icloud.com>
2024-01-10 16:45:42 -0500

    added drive to pos

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/DriveTrain.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/DriveTrain.java
@@ -112,6 +112,17 @@ public class DriveTrain implements Constants{
 
     }
 
+    public void driveToPosition(double x, double y, double power, double returnThreshhold){
+        double slope = (this.y - y) / (this.x - x); 
+        double angle = x > 0 ? Math.atan(slope) : Calculator.addAngles(Math.atan(slope), Math.PI);
+        drive(power, angle, 0, true);
+
+        if((x - returnThreshhold) < this.x && this.x < (x + returnThreshold) && (y - returnThreshhold) < this.y && this.y < (y + returnThreshold)){
+            return;
+        }
+
+    }
+
     public double getX(){
         return x;
     }
